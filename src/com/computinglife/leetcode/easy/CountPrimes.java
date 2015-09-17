fb4361b8a226841e201344615ea4e4a5cd179b1b
YoungCoding <liuyang0704@gmail.com>
2015-09-17 17:22:51 +0800

    add count prime

---
+++ src/com/computinglife/leetcode/easy/CountPrimes.java
@@ -0,0 +1,34 @@
+//Description:
+//
+//        Count the number of prime numbers less than a non-negative number, n.
+
+package com.computinglife.leetcode.easy;
+
+/**
+ * Created by Young on 2015/9/17.
+ */
+public class CountPrimes {
+    public int countPrimes(int n) {
+        boolean[] isPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+        for (int i = 2; i * i < n; i++) {
+            if (!isPrime[i])
+                continue;
+            for (int j = i * i; j < n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        CountPrimes countPrimes = new CountPrimes();
+        System.out.println(countPrimes.countPrimes(50));
+    }
+}
