040a65fcaffd6f8494687e43b4d32dfc448646ae
YoungLiu <liuyang0704@gmail.com>
2015-09-22 16:16:10 +0800

    add reverseBits

--- src/com/computinglife/leetcode/easy/ReverseBits.java
+++ src/com/computinglife/leetcode/easy/ReverseBits.java
@@ -5,41 +5,33 @@
 //
 //        Follow up:
 //        If this function is called many times, how would you optimize it?
+//note:这道题在用１做测试用例的情况下是无法解决的，因为在java中int的取值范围为-2147483648~2147483647
+//     在返回值为int的情况下是无法返回2147483648的
 
 package com.computinglife.leetcode.easy;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * Created by Young on 2015/9/20.
  */
 public class ReverseBits {
-    // you need treat n as an unsigned value
-    public int reverseBits(int n) {
-        List<Integer> original = new ArrayList<>();
-        int result = 0;
-        while (n != 0) {
-            if (n % 2 != 0) {
-                original.add(1);
-            } else {
-                original.add(0);
-            }
-            n = n >>> 1;
-        }
-        while(original.size()<32){
-            original.add(0);
-        }
-        Collections.reverse(original);
-        for (int i=0;i<32;i++) {
-            result += original.get(i) * Math.pow(2, i);
-        }
-        return result;
-    }
+	// you need treat n as an unsigned value
+	public int reverseBits(int n) {
+		int res = 0;
+		int count = 31;
+		while (n != 0) {
+			if ((n & 1) == 1) {
+				res = res + (1 << count);
+			}
+			n >>>= 1;
+			count--;
+		}
 
-    public static void main(String[] args){
-        ReverseBits test = new ReverseBits();
-        System.out.println(test.reverseBits(1));
-    }
+		return res;
+	}
+
+	public static void main(String[] args) {
+		ReverseBits test = new ReverseBits();
+		System.out.println(test.reverseBits(1));
+		// System.out.println(1 << 31);
+	}
 }
