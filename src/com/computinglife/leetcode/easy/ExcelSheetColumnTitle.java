d9df64de9ee02080a7808a77bd3eb544f936af74
YoungLiu <liuyang0704@gmail.com>
2015-09-24 20:45:44 +0800

    add excel sheet column title

--- src/com/computinglife/leetcode/easy/ExcelSheetColumnTitle.java
+++ src/com/computinglife/leetcode/easy/ExcelSheetColumnTitle.java
@@ -12,8 +12,32 @@
 
 package com.computinglife.leetcode.easy;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ExcelSheetColumnTitle {
 	public String convertToTitle(int n) {
+		char[] dictionary = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+				'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+		int tmp = 0;
+		List<Character> ret = new ArrayList<>();
+		while (n != 0) {
+			tmp = n % 26;
+			if (tmp == 0) {
+				ret.add(dictionary[25]);
+				n = n / 26 - 1;
+			} else {
+				ret.add(dictionary[tmp - 1]);
+				n /= 26;
+			}
+		}
+		Collections.reverse(ret);
+		return ret.toString().replaceAll("[\\[\\]\\s,]", "");
+	}
 
+	public static void main(String[] args) {
+		ExcelSheetColumnTitle test = new ExcelSheetColumnTitle();
+		System.out.println(test.convertToTitle(702));
 	}
 }
