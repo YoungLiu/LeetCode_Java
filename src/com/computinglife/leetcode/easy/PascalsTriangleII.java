4138f8858c7515dec1d7f039c1abf09c6310dce5
YoungLiu <liuyang0704@gmail.com>
2015-09-30 15:13:21 +0800

    add and pascal triangle II

--- src/com/computinglife/leetcode/easy/PascalsTriangleII.java
+++ src/com/computinglife/leetcode/easy/PascalsTriangleII.java
@@ -8,9 +8,41 @@
 
 package com.computinglife.leetcode.easy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by youngliu on 9/28/15.
  */
 public class PascalsTriangleII {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> ori = new ArrayList<>();
+        ori.add(0);
+        ori.add(1);
+        ori.add(0);
+        List<Integer> tmp = new ArrayList<>();
+        for (int i = 0; i < rowIndex; i++) {
+            for (int j = 0; j < ori.size(); j++) {
+                if (j == 0) {
+                    tmp.add(0);
+                }
+                if (j == ori.size() - 1) {
+                    tmp.add(ori.get(j));
+                } else {
+                    tmp.add(ori.get(j) + ori.get(j + 1));
+                }
+            }
+            ori.removeAll(ori);
+            ori.addAll(tmp);
+            tmp.removeAll(tmp);
+        }
+        return ori.subList(1, ori.size() - 1);
 
+    }
+
+    public static void main(String[] args) {
+        PascalsTriangleII test = new PascalsTriangleII();
+        System.out.println(test.getRow(2));
+
+    }
 }
