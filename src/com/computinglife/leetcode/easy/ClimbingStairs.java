673f96e76e8d169731e45576fb1f803cc563ba72
YoungCoding <liuyang0704@gmail.com>
2015-10-07 15:08:49 +0800

    add some

---
+++ src/com/computinglife/leetcode/easy/ClimbingStairs.java
@@ -0,0 +1,14 @@
+//You are climbing a stair case. It takes n steps to reach to the top.
+//
+//        Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+package com.computinglife.leetcode.easy;
+
+/**
+ * Created by Young on 2015/10/6.
+ */
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        return 0;
+    }
+}
---
+++ src/com/computinglife/leetcode/easy/RemoveDuplicatesfromSortedList.java
@@ -0,0 +1,42 @@
+//Given a sorted linked list, delete all duplicates such that each element appear only once.
+//
+//        For example,
+//        Given 1->1->2, return 1->2.
+//        Given 1->1->2->3->3, return 1->2->3.
+
+package com.computinglife.leetcode.easy;
+
+/**
+ * Created by Young on 2015/10/6.
+ */
+public class RemoveDuplicatesfromSortedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null){
+            return head;
+        }
+        ListNode tmp = head;
+        while (tmp.next != null) {
+            if (tmp.val == tmp.next.val && tmp.next.next != null) {
+                tmp.next = tmp.next.next;
+                continue;
+            }
+            if (tmp.val == tmp.next.val && tmp.next.next == null) {
+                tmp.next = null;
+                break;
+            }
+            tmp = tmp.next;
+        }
+        return head;
+    }
+
+
+}
