c5ea01efc33e5799c54fdb1e5bb28bd7541a345d
YoungLiu <liuyang0704@gmail.com>
2015-10-17 18:41:19 +0800

    add remove element

---
+++ src/com/computinglife/leetcode/easy/RemoveElement.java
@@ -0,0 +1,38 @@
+//Given an array and a value, 
+//remove all instances of that value in place and return the new length.
+//
+//The order of elements can be changed. 
+//It doesn't matter what you leave beyond the new length.
+
+package com.computinglife.leetcode.easy;
+
+public class RemoveElement {
+	public int removeElement(int[] nums, int val) {
+		int length = nums.length;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == val) {
+				length--;
+				if (length == 0) {
+					return length;
+				}
+				if (i == length) {
+					return length;
+				}
+				for (int j = i; j < length; j++) {
+					nums[j] = nums[j + 1];
+				}
+				if (i == nums.length - 1) {
+					break;
+				}
+				i--;
+			}
+		}
+		return length;
+	}
+
+	public static void main(String[] args) {
+		RemoveElement test = new RemoveElement();
+		int[] nums = { 4, 5, 5 };
+		System.out.println(test.removeElement(nums, 5));
+	}
+}
