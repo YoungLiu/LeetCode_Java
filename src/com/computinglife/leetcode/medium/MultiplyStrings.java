d467c1e31e262b9601c5364f9db1428ead83bbd6
youngliu <liuyang0704@gmail.com>
2015-11-10 13:39:01 +0800

    add multiply String

--- src/com/computinglife/leetcode/medium/MultiplyStrings.java
+++ src/com/computinglife/leetcode/medium/MultiplyStrings.java
@@ -4,13 +4,28 @@
 
 package com.computinglife.leetcode.medium;
 
-import java.math.BigInteger;
-
 public class MultiplyStrings {
 	public String multiply(String num1, String num2) {
-		BigInteger bNum1 = new BigInteger(num1);
-		BigInteger bNum2 = new BigInteger(num2);
-		bNum1 = bNum1.multiply(bNum2);
-		return bNum1.toString();
+		int[] nums = new int[num1.length() + num2.length()];
+		int len1 = num1.length();
+		int len2 = num2.length();
+		for (int i = len1 - 1; i >= 0; i--) {
+			for (int j = len2 - 1; j >= 0; j--) {
+				int temp = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+				nums[i + j] += (temp + nums[i + j + 1]) / 10;
+				nums[i + j + 1] = (nums[i + j + 1] + temp) % 10;
+			}
+		}
+		StringBuilder res = new StringBuilder();
+		for (int i : nums) {
+			if (res.length() > 0 || i > 0) {
+				res.append(i);
+			}
+		}
+		return res.length() == 0 ? "0" : res.toString();
+	}
+
+	public static void main(String[] args) {
+
 	}
 }
