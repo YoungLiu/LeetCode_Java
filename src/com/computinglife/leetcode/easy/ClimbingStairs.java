3360a52e43c84b12c66e7248d3cafdcd9fbb3f42
YoungLiu <liuyang0704@gmail.com>
2015-11-25 10:18:30 +0800

    fix climbing stairs

--- src/com/computinglife/leetcode/easy/ClimbingStairs.java
+++ src/com/computinglife/leetcode/easy/ClimbingStairs.java
@@ -8,7 +8,26 @@ package com.computinglife.leetcode.easy;
  * Created by Young on 2015/10/6.
  */
 public class ClimbingStairs {
-    public int climbStairs(int n) {
-        return 0;
-    }
+	// 此递归方法超出了运算判定时间限制
+	public int climbStairs_Time_Limit_Exceeded(int n) {
+		int[] resource = { 0, 1, 2 };
+		if (n <= 2) {
+			return resource[n];
+		}
+		return climbStairs_Time_Limit_Exceeded(n - 1) + climbStairs_Time_Limit_Exceeded(n - 2);
+	}
+
+	// 递推法(从前往后递推)
+	public int climbStairs(int n) {
+		int[] dp = { 1, 1, 0 };
+		if (n < 2) {
+			return 1;
+		}
+		for (int i = 2; i <= n; i++) {
+			dp[2] = dp[0] + dp[1];
+			dp[0] = dp[1];
+			dp[1] = dp[2];
+		}
+		return dp[2];
+	}
 }
