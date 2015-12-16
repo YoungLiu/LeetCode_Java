0c3f4555578065ff9b6544bd6c0724403f4f6a7d
YoungLiu <liuyang0704@gmail.com>
2015-12-16 10:54:25 +0800

    add rotatelist

--- src/com/computinglife/leetcode/medium/RotateList.java
+++ src/com/computinglife/leetcode/medium/RotateList.java
@@ -7,17 +7,49 @@
 package com.computinglife.leetcode.medium;
 
 public class RotateList {
-	public class ListNode {
-		int val;
-		ListNode next;
+    public class ListNode {
+        int val;
+        ListNode next;
 
-		ListNode(int x) {
-			val = x;
-		}
-	}
+        ListNode(int x) {
+            val = x;
+        }
+    }
 
-	public ListNode rotateRight(ListNode head, int k) {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null) {
+            return head;
+        }
+        if (head.next == null) {
+            return head;
+        }
+        int length = 1;
+        ListNode tmp = head;
+        while (tmp.next != null) {
+            length++;
+            tmp = tmp.next;
+        }
+        //处理k的值大于链表长度的情况
+        k %= length;
+        if (k == 0) {
+            return head;
+        }
 
-	}
-
+        ListNode first = head;
+        ListNode second = head;
+        while (first.next != null) {
+            if (k > 0) {
+                first = first.next;
+                k--;
+            } else {
+                first = first.next;
+                second = second.next;
+            }
+        }
+        //各指针完成站位
+        first.next = head;
+        head = second.next;
+        second.next = null;
+        return head;
+    }
 }
