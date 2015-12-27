f34bbd5a84ae35c0e3d7bbc508273d013f976b8e
YoungLiu <liuyang0704@gmail.com>
2015-12-27 14:33:39 +0800

    add restore ip address

--- src/com/computinglife/leetcode/medium/RestoreIPAddresses.java
+++ src/com/computinglife/leetcode/medium/RestoreIPAddresses.java
@@ -7,6 +7,7 @@
 
 package com.computinglife.leetcode.medium;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,7 +15,45 @@ import java.util.List;
  */
 public class RestoreIPAddresses {
     public List<String> restoreIpAddresses(String s) {
-        
+        List<String> res = new ArrayList<>();
+        List<String> list = new ArrayList<>();
+        if (s.length() < 4 || s.length() > 12) {
+            return res;
+        }
+        helper(res, list, s, 0);
+        return res;
+    }
+
+    public void helper(List<String> res, List<String> list, String s, int start) {
+        if (list.size() == 4) {
+            if (start != s.length()) {
+                return;
+            }
+            StringBuffer sb = new StringBuffer();
+            for (String tmp : list) {
+                sb.append(tmp);
+                sb.append(".");
+            }
+            sb.deleteCharAt(sb.length() - 1);
+            res.add(sb.toString());
+            return;
+        }
+        for (int i = start; i < s.length() && i <= start + 2; i++) {
+            String tmp = s.substring(start, i + 1);
+            if (isValid(tmp)) {
+                list.add(tmp);
+                helper(res, list, s, i + 1);
+                list.remove(list.size() - 1);
+            }
+        }
+    }
+
+    public boolean isValid(String s) {
+        if (s.charAt(0) == '0') {
+            return s.equals("0");
+        }
+        int digit = Integer.parseInt(s);
+        return digit >= 0 && digit <= 255;
     }
 
 }
