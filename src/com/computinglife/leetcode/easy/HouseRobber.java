62e9663bb0a588b27427a7c3a08804f7bb691b18
youngliu <liuyang0704@gmail.com>
2016-02-24 21:17:47 +0800

    add house robber I and II

--- src/com/computinglife/leetcode/easy/HouseRobber.java
+++ src/com/computinglife/leetcode/easy/HouseRobber.java
@@ -27,6 +27,22 @@ public class HouseRobber {
         return Math.max(rob, notRob);
     }
 
+    public int rob2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[nums.length - 1];
+    }
+
     public static void main(String[] args) {
         HouseRobber test = new HouseRobber();
         int[] nums = {50, 50, 60, 60};
--- src/com/computinglife/leetcode/medium/HouseRobberII.java
+++ src/com/computinglife/leetcode/medium/HouseRobberII.java
@@ -13,7 +13,7 @@ package com.computinglife.leetcode.medium;
  * Created by youngliu on 2/23/16.
  */
 public class HouseRobberII {
-    public int rob(int[] nums) {
+    public int rob2(int[] nums) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
@@ -39,5 +39,28 @@ public class HouseRobberII {
             dr[i] = Math.max(dr[i - 1], dr[i - 2] + nums[i]);
         }
         return Math.max(dp[len - 1], dr[len - 1]);
+    }
+
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        return Math.max(rob(nums, 0, nums.length - 1), rob(nums, 1, nums.length));
+    }
+
+    private int rob(int[] nums, int left, int right) {
+        if (right - left <= 1) {
+            return nums[left];
+        }
+        int[] dp = new int[right];
+        dp[left] = nums[left];
+        dp[left + 1] = Math.max(nums[left], nums[left + 1]);
+        for (int i = left + 2; i < right; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[right - 1];
     }
 }
